8126e977ad3badd8b80cf07460ec862f737efccf
MatiasNeira1 <matiasjasban98@gmail.com>
2025-10-06 22:43:50 -0300

    rama feature: Creacion de campos de datos de usuario para registro

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Form, Button, Card, Container, Row, Col } from "react-bootstrap";
 
-export default function register(){
+export default function Register(){
 
     const[name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -15,6 +15,56 @@ export default function register(){
         
     }
 
+  return (
+    <Container className="mt-5">
+        {/*Campo de nombre*/}
+        <Form.Group as={Row} className="mb-3" controlId="formPlaintextName"> 
+        <Form.Label column sm="3">
+        Nombre
+        </Form.Label>  
+        <Col sm="9">
+        <Form.Control type="text" placeholder="Nombre" />
+        </Col> 
+    </Form.Group>
+
+    <Form.Group as={Row} className="mb-3" controlId="formPlaintextLastName">
+        <Form.Label column sm="3">
+            Telefono
+        </Form.Label>
+        <Col sm="9">
+            <Form.Control type="text" placeholder="Telefono" />
+        </Col>
+    </Form.Group>
+
+    {/*Campo de email*/}
+  <Form>
+    <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+      <Form.Label column sm="3">
+        Correo electronico
+      </Form.Label>
+      <Col sm="9">
+        <Form.Control type="email" placeholder="correo@ejemplo.com" />
+      </Col>
+    </Form.Group>
+
+    {/*Campo de contraseña*/}
+    <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+      <Form.Label column sm="3">
+        Contraseña
+      </Form.Label>
+      <Col sm="9">
+        <Form.Control type="password" placeholder="Contraseña" />
+      </Col>
+    </Form.Group>
 
 
+  </Form>
+
+</Container>
+
+  );
 }
+
+
+
+
