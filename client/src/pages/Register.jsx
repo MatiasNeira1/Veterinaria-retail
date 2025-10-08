a7b385499c3ad2d41516899018b8a7310f5b804b
unknown <Cetecom@SJAPCA03LC0305.duoc.acad>
2025-10-08 20:17:49 -0300

    Agregacion de funcion registro

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,28 +2,42 @@ import { useState } from "react";
 import { Form, Button, Card, Container, Row, Col } from "react-bootstrap";
 
 export default function Register(){
-
+    const[users, SetUsers]=useState([]);
     const[name, setName] = useState("");
+    const[telefono,setTelefono]= useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [mensaje, setMensaje] = useState("");
+    
+    //const [mensaje, setMensaje] = useState("");
 
-    const handleRegister = (e) => {
+    const userRegister = (e) => {
         e.preventDefault();
 
+      const newUser= {name,telefono,email,password};
 
-        
+      SetUsers([users,newUser])
+      setName("");
+      setTelefono("");
+      setEmail("");
+      setPassword("");
+      
+      console.log("Email logueado:"+ {email})
     }
 
   return (
     <Container className="mt-5">
+      <form onSubmit={userRegister}>
         {/*Campo de nombre*/}
         <Form.Group as={Row} className="mb-3" controlId="formPlaintextName"> 
         <Form.Label column sm="3">
         Nombre
         </Form.Label>  
         <Col sm="9">
-        <Form.Control type="text" placeholder="Nombre" />
+        <Form.Control type="text" 
+        placeholder="Nombre"
+        value={name} 
+        onChange={(e) => setName(e.target.value)}/>
+
         </Col> 
     </Form.Group>
 
@@ -32,18 +46,27 @@ export default function Register(){
             Telefono
         </Form.Label>
         <Col sm="9">
-            <Form.Control type="text" placeholder="Telefono" />
+            <Form.Control type="text" 
+            placeholder="Telefono" 
+            value={telefono}
+            onChange={(e) => setTelefono(Number(e.target.value))}
+            />
         </Col>
     </Form.Group>
 
     {/*Campo de email*/}
-  <Form>
+ 
     <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
       <Form.Label column sm="3">
         Correo electronico
       </Form.Label>
       <Col sm="9">
-        <Form.Control type="email" placeholder="correo@ejemplo.com" />
+        <Form.Control type="email"
+         placeholder="correo@ejemplo.com"
+         value={email}
+         onChange={(e) => setEmail(e.target.value)}
+         />
+
       </Col>
     </Form.Group>
 
@@ -53,12 +76,18 @@ export default function Register(){
         Contraseña
       </Form.Label>
       <Col sm="9">
-        <Form.Control type="password" placeholder="Contraseña" />
+        <Form.Control type="password"
+        placeholder="Contraseña"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        />
       </Col>
     </Form.Group>
 
+    <Button type="submit"variant="success">Registrar</Button>
 
-  </Form>
+    </form>
+  
 
 </Container>
 
